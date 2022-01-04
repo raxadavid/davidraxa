8200f383e412e0b9342427d47be9ddbb6125c8d6
raxadavid <raxadavid@yahoo.com>
2022-01-03 17:40:00 -0800

    added scroll to top button

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
+
+//Get the button
+var mybutton = document.getElementById("topButton");
+
+// When the user scrolls down 30px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
